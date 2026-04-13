3d6febb609b32ed28254e4e887dcd8a2b489afaf
Harsha V Boorla <harsha@incubyte.co>
2026-04-14 02:16:16 +0530

    ⚙️ Setup MSW for browser usage in development

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,24 @@ import { createRoot } from 'react-dom/client'
 import '@/index.css'
 import App from '@/App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    return
+  }
+
+  const { worker } = await import('./lib/msw/browser')
+
+  // `worker.start()` returns a Promise that resolves
+  // when the Service Worker is registered and ready to intercept requests.
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
