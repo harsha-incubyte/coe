aa59c2fb95b273b3390eef1fa27cf22dc14a9826
Harsha V Boorla <harsha@incubyte.co>
2026-04-18 19:28:49 +0530

    ⚙️ config: implement babel-plugin-styled-components for readable class names

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import babel from 'vite-plugin-babel'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: [
+          '@babel/preset-typescript',
+          ['@babel/preset-react', { runtime: 'automatic' }],
+        ],
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: true,
+              ssr: false,
+              pure: true,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
