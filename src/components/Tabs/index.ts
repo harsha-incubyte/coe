fec41118890b4bd837a3659f7cdb5b8bf44a169c
Harsha V Boorla <harsha@incubyte.co>
2026-04-21 12:49:05 +0530

    ♻️ refactor(components): remove unused legacy components (Spinner, Modal, Tabs, Toast, Navbar)

--- src/components/Tabs/index.ts
+++
@@ -1,6 +0,0 @@
-export { Tabs } from './Tabs';
-export { TabsList } from './TabsList';
-export { Tab } from './Tab';
-export { TabPanel } from './TabPanel';
-export { useTabs } from './TabsContext';
-export type { TabsContext } from './TabsContext';
